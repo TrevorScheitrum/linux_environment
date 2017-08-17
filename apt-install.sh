894f8741dac7ed80cd2b05e104fa3b051cdaaa26
Trevor Scheitrum <tascheitrum@gmail.com>
2017-08-17 16:25:27 -0700

    Update apt-install.sh

--- apt-install.sh
+++ apt-install.sh
@@ -2,10 +2,6 @@
 # add required repos
 #
 sudo add-apt-repository ppa:linrunner/tlp -y
-
-# hipchat
-sudo sh -c 'echo "deb https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client $(lsb_release -c -s) main" > /etc/apt/sources.list.d/atlassian-hipchat4.list'
-wget -O - https://atlassian.artifactoryonline.com/atlassian/api/gpg/key/public | sudo apt-key add -
 sudo add-apt-repository ppa:mystic-mirage/pycharm -y
 
 # add personal related repos
@@ -28,22 +24,20 @@ echo "deb http://apt.syncthing.net/ syncthing release" | sudo tee /etc/apt/sourc
 sudo apt-get update
 
 # coding related
-sudo apt-get install -y git python python-pip pwython3 python3-pip python-dev
+sudo apt-get install -y git python python-pip python3 python3-pip python-dev
 sudo apt-get install -y build-essential
 
 # database related
 sudo apt-get install -y sqlite3 sqliteman
-sudo apt-get install -y postgresql-9.3 postgresql-client-9.3 pgadmin3
+sudo apt-get install -y pgadmin3
 
 # virtual machines related
 sudo apt-get install -y virtualbox virtualbox-dkms virtualbox-ext-pack
 sudo apt-get install -y vagrant
 sudo apt-get install -y nfs-common nfs-kernel-server
-sudo apt-get install -y qemu-kvm libvirt-bin bridget-utils virt-manager
+sudo apt-get install -y qemu-kvm libvirt-bin bridge-utils virt-manager
 
 # communications related
-sudo apt-get install -y hipchat4
-sudo apt-get install -y zoom
 
 # code editors
 sudo apt-get install -y visual-studio-code
@@ -52,6 +46,7 @@ sudo apt-get install -y guake
 
 # browsers
 sudo apt-get install -y google-chrome-stable
+sudo apt-get install -y firefox
 sudo apt-get install -y chromium-browser chromium-codecs-ffmpeg-extra
 sudo apt-get install -y vivaldi-stable
 
@@ -61,7 +56,7 @@ sudo apt-get install -y vivaldi-stable
 
 # system utils
 sudo apt-get install -y tlp tlp-rdw
-sudo apt-get install -y fluxgui
+sudo apt-get install -y redshift redshift-gtk
 sudo apt-get install -y kvpm
 sudo apt-get install -y phablet-tools ubuntu-device-flash
 sudo apt-get install -y playonlinux
